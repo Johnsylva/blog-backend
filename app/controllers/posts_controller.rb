f564bf14977c52771214e08b61970db9d544e43f
Johnsylva <90647997+Johnsylva@users.noreply.github.com>
2025-12-11 19:18:16 -0600

    Addded Jbuilder

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,45 +9,45 @@ class PostsController < ApplicationController
   #only admins can updata or destroy
 
   def index
-    posts = Post.all.order(:id)
-    render json: posts
+    @posts = Post.all.order(:id)
+    render :index
   end
 
   def create
-    post = Post.new(
+    @post = Post.new(
       title: params[:title],
       body: params[:body],
       image: params[:image],
       user_id: current_user.id
     )
-    if post.save
-      render json: post
+    if @post.save
+      render :show
     else
       render json: { errors: post.errors.full_messages }, status: :bad_request
     end
   end
 
   def show
-    post = Post.find_by(id: params[:id])
-    render json: post
+    @post = Post.find_by(id: params[:id])
+    render :show
   end
 
   def update
-    post = Post.find_by(id: params[:id])
-    post.title = params[:title] || post.title
-    post.body = params[:body] || post.body
-    post.image = params[:image] || post.image
-    post.user_id = params[:user_id] || post.user_id
-    if post.save
-      render json: post
+    @post = Post.find_by(id: params[:id])
+    @post.title = params[:title] || post.title
+    @post.body = params[:body] || post.body
+    @post.image = params[:image] || post.image
+    @post.user_id = params[:user_id] || post.user_id
+    if @post.save
+      render :show
     else
       render json: { errors: post.errors.full_messages }, status: :bad_request
     end
   end
 
   def destroy
-    post = Post.find_by(id: params[:id])
-    post.destroy
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
     render json: { message: "Post successfully destroyed!" }
   end
 end
---
+++ app/views/posts/_post.json.jbuilder
@@ -0,0 +1,8 @@
+json.id post.id
+json.body post.body
+json.image post.image
+json.title post.title
+json.created_at post.created_at
+json.updated_at post.updated_at
+json.user_id post.user_id
+json.tags post.tags
